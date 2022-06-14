7ec09fdfde0cb6a1f8c37281991dead837fd2ab3
Emre Karamik <emrekaramikjs@gmail.com>
2022-06-14 09:52:32 +0300

    ilk react kodları

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg"
+
+function App() {
+  return (
+    <div className="App">
+      <img src="/logo192.png" alt=""/>
+      <img src={logo}/>
+
+    test2
+
+
+    </div>
+  );
+}
+
+export default App;
